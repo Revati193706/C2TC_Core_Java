39445632e64aec9df911cc7c640460b7537c29db
Revati <Revati@DESKTOP-1L7DMQR>
2022-03-28 09:37:23 +0530

    If_Else Demo

---
+++ C2TC_Core_Java/src/Core_Java_Training/If_else_Demoo.java
@@ -0,0 +1,78 @@
+package Core_Java_Training;
+
+public class If_else_Demoo {
+
+	public static void main(String[] args) {
+	
+		int x=10;
+	
+		/* only boolean values are allowed in java for if condition
+		
+		if(x)
+		{
+			 System.out.println("Good Morning");
+	}
+
+		else 
+		{
+			 System.out.println("Good Afternoon");
+		}
+			
+		*/
+		
+		/*  Here we are checking condition so it checks for true or false 
+	
+		if(x==10)
+		{
+ Sysytem.out.println("Good Morning");
+	}
+
+		else 
+		{
+			 System.out.println("Good Afternoon");
+		}
+*/
+		
+				/* Assignment will not be done since it is having integer data type
+		
+		if(x=10)
+		{		
+ System.out.println("Good Morning");
+	}
+
+		else 
+		{
+			 System.out.println("Good Afternoon");
+		}
+		
+		*/
+		
+		/* assign false value to to boolean variable and will print else part (if=true).
+		
+		boolean b=true;
+		
+		if(b=false)
+		{
+			 System.out.println("Good Morning");
+		}
+		
+		else
+		{
+			System.out.println("Good Afternoon");
+		}
+		*/
+	
+	/*
+		if(true);	// Empty statement ..... valid statement. .....else and curly braces optional.
+	*/
+		
+		/*
+		if(true); 
+		int x; //No declarative /initializing statements allowed when skipping curly braces
+*/
+		
+}
+		
+}
+	
+
