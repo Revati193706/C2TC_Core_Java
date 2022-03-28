5754794e95c89b5abb306f0f5aa47c3f329f2e2f
Revati <Revati@DESKTOP-1L7DMQR>
2022-03-28 09:38:34 +0530

    String Operation Demo

---
+++ C2TC_Core_Java/src/Core_Java_Training/String_Demo.java
@@ -0,0 +1,43 @@
+package Core_Java_Training;
+
+public class String_Demo {
+
+	public static void main(String[] args) {
+
+ String s=new String ("Revati");
+
+/*  s.concat("Natekar"); // objects are Immutable does not change value.
+
+System.out.println(s);
+
+System.out.println(s.length()); // Gives string length
+
+System.out.println(s.toUpperCase()); // Converts string to Upper string
+
+System.out.println(s.toLowerCase()); // Converts string to Lower string
+
+System.out.println(s.substring(2)); // Prints a part of the substring starting from index 1.
+
+System.out.println(s.substring(1, 5)); // Prints a part of the substring starting from index 1 and prints ill 4 since we want to exclude the last index mentioned.
+
+System.out.println(s.isEmpty()); // Returns whether the string is empty or not.
+
+System.out.println(s.replace("Revati", "Reva")); //Replace Revati with Reva
+
+System.out.println(s.equalsIgnoreCase("Revati")); */
+
+// To DEcide the Operating behavior of '+' operator 
+
+String a="Java";
+int b=10,c=20,d=30;
+
+System.out.println(a+b+c+d); // Because a is string so acts as concatenation = java102030
+
+System.out.println(b+c+d+a); // Because b is int so acts as arithmatic operator = 60Java
+
+System.out.println(b+c+a+d); //Because b is int so acts as arithmatic operator = 30Java30
+
+System.out.println(b+a+c+d); //Because b is int so acts as arithmatic operator = 10Java2030
+	}
+
+}
