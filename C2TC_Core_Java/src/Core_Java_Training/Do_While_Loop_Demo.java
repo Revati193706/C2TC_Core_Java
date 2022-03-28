0f60dfd34be680f8c5bae879ebac92f727601cd6
Revati <Revati@DESKTOP-1L7DMQR>
2022-03-28 09:37:05 +0530

    Demo Do-While Loop

---
+++ C2TC_Core_Java/src/Core_Java_Training/Do_While_Loop_Demo.java
@@ -0,0 +1,43 @@
+package Core_Java_Training;
+
+public class Do_While_Loop_Demo {
+
+	public static void main(String[] args) {
+		
+		/*
+		
+do
+	
+{
+	
+	System.out.println("Hello"); //infinite loop condition always true
+	
+}while(true);
+
+*/
+
+/* do;
+while(true);  //curley braces not needed */
+
+/*
+
+while(true)
+{
+	//empty body allowed
+		
+}*/
+	
+		
+/*
+
+		do
+			
+		{
+			
+		} while(1); // boolean value needed
+		
+		*/
+		
+}
+	
+}
