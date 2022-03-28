65b39a5e04feb0641f5f48b00be5ad6fcd8633fb
Revati <Revati@DESKTOP-1L7DMQR>
2022-03-28 09:37:41 +0530

    Increment Operator Demo

---
+++ C2TC_Core_Java/src/Core_Java_Training/Increment_operator_demo.java
@@ -0,0 +1,42 @@
+package Core_Java_Training;
+
+public class Increment_operator_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int a=10;
+	
+	System.out.println("pre increment \n");
+	
+System.out.println("the initial x="+a);
+	
+int b;
+
+b=++a;
+
+System.out.println("the value of y="+b);
+
+System.out.println("the value of y="+a);
+
+System.out.println("\n");
+
+
+int x=10;
+
+System.out.println("post increment \n");
+
+System.out.println("the initial x="+x);
+
+int y;
+
+y=x++;
+
+System.out.println("the value of y="+y);
+
+System.out.println("the value of y="+x);	
+
+
+}
+	
+}
