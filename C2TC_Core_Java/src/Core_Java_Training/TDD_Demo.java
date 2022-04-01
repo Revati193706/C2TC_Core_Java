8c1e152fa731f1320088b1ec4ee023babeac6ccc
Revati <Revati@DESKTOP-1L7DMQR>
2022-04-01 11:54:01 +0530

    Demo adding j-unit library for testing.

---
+++ C2TC_Core_Java/src/Core_Java_Training/TDD_Demo.java
@@ -0,0 +1,28 @@
+package Core_Java_Training;
+
+import org.junit.jupiter.api.*;
+
+public class TDD_Demo {
+
+	@Disabled
+	public void sum() 
+	{
+		int a=10;
+		int b=20;
+		
+		int result=a+b;
+		System.out.println(result);
+	}
+	
+	@Test
+	void display()
+	{
+		System.out.println("Inside Test Driven Developement");
+	}
+	
+	public static void main(String[] args) {
+
+	
+	}
+
+}
