b128b87c34b92e905ee7445d8d993a7f208cd6bb
Revati <Revati@DESKTOP-1L7DMQR>
2022-04-08 10:41:30 +0530

    Collections and Generics Demo

---
+++ C2TC_Core_Java/src/Core_Java_Training/Collection_Demo.java
@@ -0,0 +1,43 @@
+package Core_Java_Training;
+
+import java.util.*;
+
+public class Collection_Demo {
+
+	public static void main(String[] args) {
+	
+		ArrayList l=new ArrayList();
+		
+		l.add(23); // Elements in the list
+		l.add(null); //Elements in the list
+		l.add("M11"); //Elements int he list
+	
+		
+		System.out.println(l.getClass()); // displays class name class java.util.ArrayList
+		
+		System.out.println(	l.size()); //Displays the size of collection
+		System.out.println(	l.get(0)); //Displays element present at the 0 index
+		System.out.println(	l); //Displays the list
+	
+		String [] arr=new String[3];
+
+		arr[0]="Revati";
+		arr[1]="Shweta";
+		
+	/*	arr[2]=10; */ // this will give error since arrays provide type safety i.e once we have declare array name as string we cannot change the data type of that variable.
+		
+		             ArrayList l1=new ArrayList(); // We cannot provide data type here. This can accept data of any type so doesnt provide type saftey.
+	
+		             /*String name0=l1.get(0);  here we need to do type casting collections doesn't provide type safety as arrays.  */ 
+		             String name0=(String) l1.get(0); 
+		             
+		            /* String name1=l1.get(1); */
+		               String name1=(String) l1.get(1); 
+		               
+		             /*  int name2=l1.get(1);*/
+		               int name2=(int) l1.get(2); // Will accpet the same and thus doesn't provide type safety so java 1.5 introduced generics
+
+	
+	}
+
+}
---
+++ C2TC_Core_Java/src/Core_Java_Training/Generics_Demo.java
@@ -0,0 +1,29 @@
+package Core_Java_Training;
+
+public class Generics_Demo {
+
+	public static void main(String[] args)
+	{
+		gen<Integer> g = new gen<Integer>();
+		gen<Float> g1 = new gen<Float>(); // Not here <> this must be a wrapper class/T
+
+		g.value = 2;
+		g1.value=2.5f;
+		
+		g1.display();
+		g.display();
+		
+	}
+}
+
+class gen<T> // Here T is data type generics allows us to provide different datatype for single generics
+{
+
+	T value;
+
+	void display() {
+		System.out.println(value);
+	}
+
+}
+
