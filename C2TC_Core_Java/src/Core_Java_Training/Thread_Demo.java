100908ac72c7a6c36ddb0ba26e3dc3c7b5b2cf4b
Revati <Revati@193706>
2022-04-11 09:07:32 +0530

    Thread Demo

--- C2TC_Core_Java/src/Core_Java_Training/Thread_Demo.java
+++ C2TC_Core_Java/src/Core_Java_Training/Thread_Demo.java
@@ -1,20 +1,29 @@
 package Core_Java_Training;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Thread_Demo extends Thread //Thread1
 
 {
-
+	
+	
 	public void run()
 	{
 		for(int i=0;i<=3;i++)
 		{
 			System.out.println("I am Thread1");
 		}
+		
+	
 	}
 	
+
 	public static void main(String[] args) //Thread2
 	
 	{
+		ReentrantLock l=new ReentrantLock();		
+		l.lock();
+		
 		System.out.println(Thread.currentThread().getThreadGroup().getParent().getName());
 		//Display currentThread,name,parent,group
 		
@@ -23,13 +32,25 @@ public class Thread_Demo extends Thread //Thread1
 		Thread t=new Thread(r);	
 		
 		t.start();
+			
+		System.out.println(l.isLocked()); 
+		System.out.println(l.getQueueLength());
 		
 	for(int i=0;i<=3;i++)
 	{
 		System.out.println("I am  Main Thread");
 	}
-	
+
 }
+	
+	
+
+	public void display()
+	{
+		System.out.println("Hello");
+	}
+	
+	
 	
 }
 
